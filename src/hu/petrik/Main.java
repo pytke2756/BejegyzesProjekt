1aa739fc2ea7d8fc52fe920012bae3b0ea9f3253
Ibrányi Levente <ibranyilevente@gmail.com>
2021-09-29 14:58:53 +0200

    Main kettő bejegyzés listába

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,16 @@
 package hu.petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Bejegyzes> bejegyzesekLista = new ArrayList<>();
+        Bejegyzes b1 = new Bejegyzes("Szerző első", "Nagyon sok tartalom");
+        Bejegyzes b2 = new Bejegyzes("Szerző második", "Annál is több tartalom");
+        bejegyzesekLista.add(b1);
+        bejegyzesekLista.add(b2);
+
     }
 }
